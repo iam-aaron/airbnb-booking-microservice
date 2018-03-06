c44835706e068b0f05354cb657b459dddc22a7b1
stephfunny <contactstephanief@gmail.com>
2018-03-05 22:02:30 -0800

    set up basic server and router, set up database model and seed initial data for listings

--- database/model.js
+++ database/model.js
@@ -17,65 +17,31 @@ var BookingSchema = mongoose.Schema({
   'listing_weekend_price_native': Number,
   'cleaning_fee_native': Number,
   'star_rating': Number,
+  'reviews_count': Number,
+  'weekly_price_factor': Number,
   'listing_price_for_extra_person_native': Number,
 
   'available_days': Array,
 });
-	
+  
 const BookingModel = mongoose.model('Booking', BookingSchema);
 
 var removeAll = function(callback) {
   BookingModel.remove({}, callback);
 };
 
-// function insertOne(data, callback) {
-// 	BookingModel.create(data, callback)
-// };
-
 var insertOne = function(data) {
   return BookingModel.create(data);
 };
 
-// function findAll(callback) {
-// 	BookingModel.find({})
-// 		.exec(callback);
-// };
-
 var findAll = function() {
   return BookingModel.find({});
-  // .exec(callback);
 };
-
-// function findOne(id, callback) {
-//   BookingModel.find({id: id}, callback);
-// }
 
 var findOne = function(roomid) {
   console.log('find one called');
   return BookingModel.findOne({'id': roomid})
     .exec();
-};
-
-// function updateavailable_days(whereKey, equalsValue, updateKey, toValue, callback) {
-//   BookingModel.where({null : null})
-// 		.updateMany({updateKey : toValue})
-// 		.exec(callback);
-// };
-
-var fakeAvailableDays = ['2/28/2018', '3/4/2018', '3/5/2018', 
-  '3/7/2018', '3/18/2018', '3/19/2018', 
-  '3/20/2018', '3/21/2018', '3/22/2018', 
-  '3/23/2018', '4/9/2018', '4/10/2018'];
-
-// function updateavailable_days(whereKey, equalsValue, toValue, callback) {
-// 	BookingModel.where({whereKey: equalsValue})
-// 	    .updateMany({'available_days' : toValue})
-// 	    .exec(callback);
-// };
-
-var updateAllavailable_days = function(toValue) {
-  return BookingModel.where({})
-    .updateMany({'available_days' : toValue});
 };
 
 module.exports = {
@@ -85,6 +51,5 @@ module.exports = {
   insertOne: insertOne,
   findAll: findAll,
   findOne: findOne,
-  updateAllavailable_days: updateAllavailable_days
 };
 
--- database/seed.js
+++ database/seed.js
@@ -1,27 +1,32 @@
 let mongoose = require('mongoose');
 let Bookings = require('./model.js');
-var data = JSON.parse(require('./allListings.json'));
+let data = JSON.parse(require('./allListings.json'));
+let Promise = require('bluebird');
 
-var fakeAvailableDays = ['2/28/2018', '3/4/2018', '3/5/2018', 
-  '3/7/2018', '3/18/2018', '3/19/2018', 
-  '3/20/2018', '3/21/2018', '3/22/2018', 
-  '3/23/2018', '4/9/2018', '4/10/2018'
+var sampleAvailableDays = [
+  new Date(2018, 2, 7), new Date(2018, 2, 8), new Date(2018, 2, 9), 
+  new Date(2018, 2, 11), new Date(2018, 2, 12), new Date(2018, 2, 13),
+  new Date(2018, 2, 15), new Date(2018, 2, 16), new Date(2018, 2, 19),
+  new Date(2018, 2, 20), new Date(2018, 2, 21), new Date(2018, 2, 22),
+  new Date(2018, 2, 25), new Date(2018, 2, 26), new Date(2018, 3, 1),
+  new Date(2018, 3, 2), new Date(2018, 3, 3), new Date(2018, 3, 4),
+  new Date(2018, 3, 5), new Date(2018, 3, 6), new Date(2018, 3, 7),
+  new Date(2018, 3, 8), new Date(2018, 3, 9), new Date(2018, 3, 10),
+  new Date(2018, 3, 11), new Date(2018, 3, 12), new Date(2018, 3, 13),
 ];
 
-var insertOne = function(data) {
-  return BookingModel.create(data);
-};
 
 var seedDb = function(data) {
   mongoose.connect('mongodb://localhost/airbnb_bookings')
-    .then(connection => {
+    .then((connection) => {
+      connect = connection;
       var BookingModel = Bookings.BookingModel;
       mongoose.Promise.map(data, (booking) => {
-        booking.listing.available_days = fakeAvailableDays;
+        booking.listing.available_days = sampleAvailableDays;
         return BookingModel.create(booking.listing);
       })
         .then(() => {
-          return connection.disconnect();
+          return connect.disconnect();
         })
         .catch((err) => console.log('Error: listing insert', err));
     })
@@ -31,11 +36,3 @@ var seedDb = function(data) {
 };
 
 seedDb(data);
-
-
-
-
-
-
-
-
