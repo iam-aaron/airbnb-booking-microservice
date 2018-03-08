c744378c80f87420d57fd7be048f34b10a4b451b
stephfunny <contactstephanief@gmail.com>
2018-03-07 16:27:44 -0800

    update css

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser');
-var bookingRouter = require('./router.js');
+var proxyRouter = require('./router.js');
 var morgan = require('morgan');
 
 app.use(express.static(__dirname + '/../public'));
@@ -23,7 +23,7 @@ app.use(function(req, res, next) {
 
 var port = 3003;
 
-app.use('/rooms', bookingRouter);
+app.use('/rooms', proxyRouter);
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
